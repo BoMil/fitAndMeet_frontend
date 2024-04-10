962165b1022db1d234a3c6b89b555b8c061f429e
BoMil <bojanmilicevic91@gmail.com>
2024-04-10 20:04:56 +0200

    FMA-74 Added new company cards

--- src/app/components/company-info-box/company-info-box.component.ts
+++ src/app/components/company-info-box/company-info-box.component.ts
@@ -4,10 +4,11 @@ import { Company } from 'src/app/_models/company';
 @Component({
   selector: 'app-company-info-box',
   templateUrl: './company-info-box.component.html',
-  styleUrls: ['./company-info-box.component.scss']
+  styleUrls: ['./company-info-box.component.scss', '../../../styles/_cards.scss']
 })
 export class CompanyInfoBoxComponent {
     @Input() company!: Company;
     @Input() defaultImage: string | null = '../../../assets/photo_placeholder.jpg';
-
+    reviewScore: number = 4; // TODO: Add real review property
+    isLiked: boolean = true; // TODO: Add real property
 }
